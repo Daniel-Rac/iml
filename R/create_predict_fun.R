48c5b59c2c4c8320632468aef85e4c6100b205f1
Daniel Racek <dnlrck95@gmail.com>
2021-12-13 23:00:03 +0100

    xxx

--- R/create_predict_fun.R
+++ R/create_predict_fun.R
@@ -34,15 +34,16 @@ create_predict_fun.Learner <- function(model, task, predict.fun = NULL, type = N
   if (task == "classification") {
     function(newdata) {
       if (model$predict_type == "response") {
-        pred <- predict(model, newdata = newdata, task_benchmark)
+        pred <- predict(model, newdata = newdata)
         factor_to_dataframe(pred)
       } else {
         data.frame(predict(model, newdata = newdata, predict_type = "prob"), check.names = FALSE)
+        print("xxx")
       }
     }
   } else if (task == "regression") {
     function(newdata) {
-      data.frame(predict(model, newdata = newdata, task_benchmark))
+      data.frame(predict(model, newdata = newdata))
     }
   } else {
     stop(sprintf("Task type '%s' not supported", task))
