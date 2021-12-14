531efe1b86b854016629874446c0665e9e16e23f
Daniel Racek <dnlrck95@gmail.com>
2021-12-14 10:43:23 +0100

    Fix

--- R/create_predict_fun.R
+++ R/create_predict_fun.R
@@ -34,17 +34,18 @@ create_predict_fun.Learner <- function(model, task, predict.fun = NULL, type = N
   if (task == "classification") {
     function(newdata) {
       if (model$predict_type == "response") {
-        pred <- predict(model, newdata = newdata)
+        #pred <- predict(model, newdata = newdata)
+        pred = learner$predict_newdata(newdata, task_benchmark)[[model$predict_type]]
         factor_to_dataframe(pred)
       } else {
-        print("yyy")
-        data.frame(predict(model, newdata = newdata, predict_type = "prob"), check.names = FALSE)
-        print("xxx")
+        #data.frame(predict(model, newdata = newdata, predict_type = "prob"), check.names = FALSE)
+        pred = data.frame(learner$predict_newdata(newdata, task_benchmark)[[model$predict_type]], check.names = FALSE)
       }
     }
   } else if (task == "regression") {
     function(newdata) {
-      data.frame(predict(model, newdata = newdata))
+      #data.frame(predict(model, newdata = newdata))
+      data.table(learner$predict_newdata(newdata, task_benchmark)) #--> still need to test this
     }
   } else {
     stop(sprintf("Task type '%s' not supported", task))
